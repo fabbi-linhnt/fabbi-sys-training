c1ab8c0142af019d56eb006d4a66acb56edf4e9b
khanhpv <khanhpv2@fabbi.io>
2020-12-01 10:03:24 +0700

    Update API base response

---
+++ app/Enums/ResponseStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class ResponseStatus extends Enum
+{
+    const INTERNAL_SERVER_ERROR = 500;
+    const OK = 200;
+}
