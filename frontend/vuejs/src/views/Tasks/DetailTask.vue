6fc6fc8ffc5667e668bac700bbe056d7afc69416
ducnh-fabbi <ducnh@fabbi.io>
2020-12-04 14:18:09 +0700

    #9: fix detail-task/detail-user

--- frontend/vuejs/src/views/Tasks/DetailTask.vue
+++ frontend/vuejs/src/views/Tasks/DetailTask.vue
@@ -1,105 +1,184 @@
 <template>
-  <div class="main-content">
-    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8"></base-header>
-    <div class="content">
-      <h3>{{ $t("task_screen.label.detail_task") }}</h3>
-      <div class="form-group">
-        <label>{{ $t("task_screen.label.task_name") }}:</label> {{ task.name }}
+  <div>
+    <div>
+      <projects-table></projects-table>
+    </div>
+    <div class="container-fluid mt-2">
+      <div class="row">
+        <div class="col">
+          <div class="card shadow">
+            <div class="card-header border-0">
+              <div class="row align-items-center">
+                <div class="col">
+                  <h3>{{ $t("task_screen.label.detail_task") }}</h3>
+                </div>
+              </div>
+            </div>
+            <div class="content-detail-task">
+              <div class="form-group">
+                <label>{{ $t("task_screen.label.task_name") }}:</label>
+                {{ task.name }}
+              </div>
+              <div class="form-group">
+                <label>{{ $t("task_screen.label.task_description") }}:</label>
+                {{ task.description }}
+              </div>
+              <div class="form-group">
+                <label>{{ $t("task_screen.label.task_content") }}:</label>
+                <p class="task-content">
+                  {{ task.content }}
+                </p>
+              </div>
+              <div class="form-group">
+                <label>{{ $t("task_screen.label.task_deadline") }}:</label>
+                {{ task.deadline }}
+              </div>
+              <div class="form-group">
+                <label>{{ $t("task_screen.label.task_isActive") }}: </label>
+                <span>
+                  {{
+                    task.is_active == 1
+                      ? $t("task_screen.label.task_active")
+                      : $t("task_screen.label.task_inactive")
+                  }}
+                </span>
+              </div>
+              <br />
+              <div class="form-group">
+                <label>{{ $t("list_users.title.list_user") }}</label>
+                <b-table striped hover :items="users" :fields="userField">
+                  <template #cell(index)="row">
+                    {{
+                      ++row.index +
+                      (Number(paginateTask.page) - 1) * Number(paginateTask.perPage)
+                    }}
+                  </template>
+                </b-table>
+              </div>
+              <div class="pagination">
+                <b-pagination
+                  v-model="paginateSubject.page"
+                  :total-rows="paginateSubject.total"
+                  :per-page="paginateSubject.perPage"
+                  aria-controls="my-table"
+                  @change="changePageSubject(paginateSubject.page)"
+                ></b-pagination>
+              </div>
+              <br />
+              <div class="form-group">
+                <label>{{ $t("list_subjects.title.list_subjects") }}</label>
+                <b-table
+                  striped
+                  hover
+                  :items="subjects"
+                  :fields="subjectsField"
+                >
+                  <template #cell(index)="row">
+                    {{
+                      ++row.index +
+                      (Number(paginateTask.page) - 1) * Number(paginateTask.perPage)
+                    }}
+                  </template>
+                </b-table>
+              </div>
+              <div class="pagination">
+                <b-pagination
+                  v-model="paginateSubject.page"
+                  :total-rows="paginateSubject.total"
+                  :per-page="paginateSubject.perPage"
+                  aria-controls="my-table"
+                  @change="changePageSubject(paginateSubject.page)"
+                ></b-pagination>
+              </div>
+              <router-link class="btn btn-primary" :to="{ name: 'tasks.list' }">
+                <i class="fas fa-undo-alt"> </i>
+                {{ $t("task_screen.label.back_home") }}
+              </router-link>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label>{{ $t("task_screen.label.task_description") }}:</label> {{ task.description }}
-      </div>
-      <div class="form-group">
-        <label>{{ $t("task_screen.label.task_content") }}:</label>
-        <p class="task-content">
-          {{ task.content }}
-        </p>
-      </div>
-      <div class="form-group">
-        <label>{{ $t("task_screen.label.task_deadline") }}:</label> {{ task.deadline }}
-      </div>
-      <div class="form-group">
-        <label>{{ $t("task_screen.label.task_isActive") }}: </label>
-        <span>
-          {{ task.is_active == 1 ? $t("task_screen.label.task_active") : $t("task_screen.label.task_inactive") }}
-        </span>
-      </div>
-      <hr>
-      <div class="form-group">
-        <label>{{ $t("list_users.title.list_user") }}</label>
-        <b-table striped hover :items="users" :fields="userField">
-          <template #cell(index)="row">
-            {{ ++row.index }}
-          </template>
-        </b-table>
-      </div>
-      <hr>
-      <div class="form-group">
-        <label>{{ $t("list_subjects.title.list_subjects") }}</label>
-        <b-table striped hover :items="subjects" :fields="subjectsField" >
-          <template #cell(index)="row">
-            {{ ++row.index }}
-          </template>
-        </b-table>
-      </div>
-      <router-link class="btn btn-primary" :to="{ name : 'tasks.list' }">
-        <i class="fas fa-undo-alt"> </i>
-        {{ $t("task_screen.label.back_home") }}
-      </router-link>
     </div>
   </div>
 </template>
 
 <script>
+import { DEFAULT_PERPAGE_USER, DEFAULT_PAGE } from "@/definition/constants";
+import ProjectsTable from "@/layout/HeaderCard";
 require("@/sass/modules/detail-task.css");
 
 export default {
+  components: {
+    ProjectsTable,
+  },
   name: "DetailTask",
   data() {
     return {
       task: [],
+      paginateSubject: {
+        page: DEFAULT_PAGE,
+        perPage: DEFAULT_PERPAGE_USER,
+        total: "",
+      },
+      paginateUser: {
+        page: DEFAULT_PAGE,
+        perPage: DEFAULT_PERPAGE_USER,
+        total: "",
+      },
       users: [],
       subjects: [],
       userField: [
-        { key: 'index', label: this.$i18n.t("common.label.index") },
-        { key: 'name', label: this.$i18n.t("user_screen.label.name") },
-        { key: 'email', label: this.$i18n.t("user_screen.label.email") },
-        { key: 'phone', label: this.$i18n.t("user_screen.label.phone_number") },
+        { key: "index", label: this.$i18n.t("common.label.index") },
+        { key: "name", label: this.$i18n.t("user_screen.label.name") },
+        { key: "email", label: this.$i18n.t("user_screen.label.email") },
+        { key: "phone", label: this.$i18n.t("user_screen.label.phone_number") },
       ],
       subjectsField: [
-        { key: 'index', label: this.$i18n.t("common.label.index") },
-        { key: 'name', label: this.$i18n.t("list_subjects.label.name") },
-        { key: 'description', label: this.$i18n.t("list_subjects.label.description") }
-      ]
-    }
+        { key: "index", label: this.$i18n.t("common.label.index") },
+        { key: "name", label: this.$i18n.t("list_subjects.label.name") },
+        {
+          key: "description",
+          label: this.$i18n.t("list_subjects.label.description"),
+        },
+      ],
+    };
   },
-  props: [
-    'id'
-  ],
+  props: ["id"],
   created() {
     this.getTask();
     this.getSubjectOfTask();
     this.getUsersOfTask();
   },
   methods: {
+    changePageSubject(page) {
+      this.paginateSubject.page = page;
+      this.getSubjectOfTask();
+    },
+    changePageUser(page) {
+      this.paginateUser.page = page;
+      this.getUserOfTask();
+    },
     async getTask() {
-      await this.$store.dispatch("task/GET_TASK_BY_ID", this.id)
-        .then(response => {
+      await this.$store
+        .dispatch("task/GET_TASK_BY_ID", this.id)
+        .then((response) => {
           this.task = response;
-        })
+        });
     },
     async getUsersOfTask() {
-      await this.$store.dispatch("task/GET_USERS_OF_TASK", this.id)
-        .then(response => {
-          this.users = response.data
-        })
+      await this.$store
+        .dispatch("task/GET_USERS_OF_TASK", this.id)
+        .then((response) => {
+          this.users = response.data;
+        });
     },
     async getSubjectOfTask() {
-      await this.$store.dispatch("task/GET_SUBJECTS_OF_TASK", this.id)
-        .then(response => {
+      await this.$store
+        .dispatch("task/GET_SUBJECTS_OF_TASK", this.id)
+        .then((response) => {
           this.subjects = response;
-        })
-    }
-  }
-}
+        });
+    },
+  },
+};
 </script>
